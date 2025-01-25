04be40097e25415d2a55d938dcc799df3e07c473
Dean Attias <dean.attias@gmail.com>
2025-01-25 16:18:09 +0200

    2948-make-lexicographically-smallest-array-by-swapping-elements

---
+++ daily-challenges/2948-make-lexicographically-smallest-array-by-swapping-elements.py
@@ -0,0 +1,32 @@
+class Solution:
+    def lexicographicallySmallestArray(self, nums: List[int], limit: int) -> List[int]:
+        nums_sorted = sorted(nums)
+
+        curr_group = 0
+        num_to_group = {}
+        num_to_group[nums_sorted[0]] = curr_group
+
+        group_to_list = {}
+        group_to_list[curr_group] = deque([nums_sorted[0]])
+
+        for i in range(1, len(nums)):
+            if abs(nums_sorted[i] - nums_sorted[i - 1]) > limit:
+                # New group
+                curr_group += 1
+
+            # Assign current element to group
+            num_to_group[nums_sorted[i]] = curr_group
+
+            # Add element to sorted group deque
+            if curr_group not in group_to_list:
+                group_to_list[curr_group] = deque()
+            group_to_list[curr_group].append(nums_sorted[i])
+
+        # Iterate through input and overwrite each element with
+        # the next element Iin its corresponding group
+        for i in range(len(nums)):
+            num = nums[i]
+            group = num_to_group[num]
+            nums[i] = group_to_list[group].popleft()
+
+        return nums
