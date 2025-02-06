c35a858c0cb7b1163ec71fb92b9ff58c9ef01be5
Dean Attias <dean.attias@gmail.com>
2025-02-06 10:24:18 +0200

    1726-tuple-with-same-product

---
+++ daily-challenges/1726-tuple-with-same-product.py
@@ -0,0 +1,39 @@
+class Solution:
+    def tupleSameProduct(self, nums: List[int]) -> int:
+        nums_length = len(nums)
+
+        pair_products = []
+
+        total_number_of_tuples = 0
+
+        # Iterate over nums to calculate all pairwise products
+        for i in range(nums_length):
+            for j in range(i + 1, nums_length):
+                pair_products.append(nums[i] * nums[j])
+
+        pair_products.sort()
+
+        last_product_seen = -1
+        same_product_count = 0
+
+        # Iterate over pair_products to count how many times each product occurs
+        for product_index in range(len(pair_products)):
+            if pair_products[product_index] == last_product_seen:
+                # Increment the count of same products
+                same_product_count += 1
+            else:
+                # Calculate how many pair shad the previous product value
+                pairs_of_equal_product = (
+                    (same_product_count - 1) * same_product_count // 2
+                )
+                total_number_of_tuples += 8 * pairs_of_equal_product
+
+                # Update last_product_seen and reset same_product_count
+                last_product_seen = pair_products[product_index]
+                same_product_count = 1
+
+        # Handle the last group of products (since the loop ends without adding it)
+        pairs_of_equal_product = (same_product_count - 1) * same_product_count // 2
+        total_number_of_tuples += 8 * pairs_of_equal_product
+
+        return total_number_of_tuples
